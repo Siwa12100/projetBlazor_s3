c02992b9dad05f8cb551a5e8ce3b1c7d3cab9d14
Siwa12100 <jean.marcillac12@gmail.com>
2023-12-04 17:22:05 +0100

    :dizzy: ajout de la première partie du tp

---
+++ projetBlazorSiwa/projetBlazorSiwa/Pages/List.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using projetBlazorSiwa.Models;
+
+namespace projetBlazorSiwa.Pages
+{
+    public partial class List
+    {
+        private Item[] items;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            items = await Http.GetFromJsonAsync<Item[]>($"{NavigationManager.BaseUri}fake-data.json");
+        }
+
+    }
+}
